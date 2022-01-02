378a89f394a0dfe653726c9f7378b1dc4b4322ec
codeComet <unknownmind10@gmail.com>
2022-01-02 18:01:45 +0600

    getting address

--- start/contract.js
+++ start/contract.js
@@ -29,10 +29,80 @@ The `piggybankContract` is compiled from:
       }
   }
 */
-
-const forwarderOrigin = 'http://localhost:9010'
+const sdk = require("api")("@opensea/v1.0#19a5bbq7kww6d5ob");
+const forwarderOrigin = "http://localhost:9010";
 
 const initialize = () => {
-  //You will start here 
-}
-window.addEventListener('DOMContentLoaded', initialize)
+  const onboardButton = document.getElementById("connectButton");
+  const getAccountsButton = document.getElementById("getAccounts");
+  const getAccountsResult = document.getElementById("getAccountsResult");
+  const nftBtn = document.getElementById("getnfts");
+
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  //We create a new MetaMask onboarding object to use in our app
+  const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+
+  //This will start the onboarding proccess
+  const onClickInstall = () => {
+    onboardButton.innerText = "Onboarding in progress";
+    onboardButton.disabled = true;
+    //On this object we have startOnboarding which will start the onboarding process for our end user
+    onboarding.startOnboarding();
+  };
+
+  const onClickConnect = async () => {
+    try {
+      // Will open the MetaMask UI
+      // You should disable this button while the request is pending!
+      await ethereum.request({ method: "eth_requestAccounts" });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const MetaMaskClientCheck = () => {
+    //Now we check to see if MetaMask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      onboardButton.innerText = "Click here to install MetaMask!";
+
+      onboardButton.onclick = onClickInstall;
+      //The button is now disabled
+      onboardButton.disabled = false;
+    } else {
+      //If it is installed we change our button text
+      onboardButton.innerText = "Connect";
+      //When the button is clicked we call this function to connect the users MetaMask Wallet
+      onboardButton.onclick = onClickConnect;
+      //The button is now disabled
+      onboardButton.disabled = false;
+    }
+  };
+  MetaMaskClientCheck();
+
+  //Eth_Accounts-getAccountsButton
+  getAccountsButton.addEventListener("click", async () => {
+    //we use eth_accounts because it returns a list of addresses owned by us.
+    const accounts = await ethereum.request({ method: "eth_accounts" });
+    //We take the first address in the array of addresses and display it
+    getAccountsResult.innerHTML = accounts[0] || "Not able to get accounts";
+    console.log(accounts[0]);
+  });
+
+  nftBtn.addEventListener("click", async () => {
+    //https://api.opensea.io/api/v1/assets?owner=${address}
+
+    await sdk["getting-assets"]({
+      order_direction: "desc",
+      offset: "0",
+      limit: "20",
+    })
+      .then((res) => console.log(res))
+      .catch((err) => console.error(err));
+  });
+};
+window.addEventListener("DOMContentLoaded", initialize);
